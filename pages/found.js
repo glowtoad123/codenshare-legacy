b594456bc7422b08212ad7e429335d0219fd88fd
Alonzo Austin <alonzo6546@outlook.com>
2020-10-12 16:32:54 -0400

    fixed bug that showed wrong full project pages when opened from 'found' page

--- pages/found.js
+++ pages/found.js
@@ -7,16 +7,14 @@ import Navbar from './navbar'
 import styles from './css/project.module.css'
 
 export default function found(){
-
     var serverClient = new faunadb.Client({ secret: 'fnADpgTNT1ACEiUC4G_M5eNjnIPvv_eL99-n5nhe' });
-
     const [projectArray, setProjectArray] = useState([])
+    const [wholeProjects, setWholeProjects] = useState({})
     const [projectIdArray, setProjectIdArray] = useState([])
     const [queriedList, setQueriedList] = useState([])
     const [selection, setSelection] = useState("")
     const [foundStatus, setFoundStatus] = useState(false)
 
-    
     const router = useRouter()
     const searchedInfo = router.query.title
     
@@ -27,65 +25,68 @@ export default function found(){
     foundStatus && localForage.setItem("foundStatus", false).then(
         router.reload()
     )
-    
 
-    console.log(searchedInfo)
+    localForage.getItem("Selection").then(ret => setSelection(ret))
 
-    projectArray.length === 0 && serverClient.query(
+    queriedList.length === 0 && serverClient.query(
         q.Map(
             q.Paginate(q.Match(q.Index("projects"))),
             q.Lambda("X", q.Get(q.Var("X")))
           )
     ).then(ret => {
-        setProjectArray(ret.data.map(project => project.data)),
-        console.log(ret.data.map(project => project.data)),
-        setProjectIdArray(ret.data.map(project => project.ref.id))
+        if(selection === "title"){
+            const results = ret.data.filter(projects => {
+                return projects.data.Project_Title.includes(searchedInfo)
+            })
+
+            console.log(results)
+
+            if(results.length > 0 && queriedList.length === 0){
+                setQueriedList(results)
+            }
+        }
+
+        if(selection === "description"){
+            const results = ret.data.filter(projects => {
+                return projects.data.Description.includes(searchedInfo)
+            })
+
+            if(results.length > 0 && queriedList.length === 0){
+                setQueriedList(results)
+            }
+        }
+
+        if(selection === "categories"){
+            const searchCategoriesList = searchedInfo.split(" ")
+            const searchResults = searchCategoriesList.map(one => 
+                ret.data.filter(projects => 
+                    projects.data.Categories.includes(one)
+                )
+            )
+
+            const finalResults = []
+            const enhancedResults = searchResults.map(queriedProjectLists => queriedProjectLists.filter(project => finalResults.push(project)))
+            if(finalResults.length > 0 && queriedList.length == 0){
+                setQueriedList(finalResults)
+                console.log(finalResults)
+            }
+        }
     })
-
-    localForage.getItem("Selection").then(ret => setSelection(ret))
-
-    if(selection === "title"){
-        const results = projectArray.filter(project => {return project.Project_Title.includes(searchedInfo)})
-        if(results.length > 0 && queriedList.length == 0){
-            setQueriedList(results)
-            console.log(results)
-        }
-    }
-
-    if(selection === "categories"){
-        const searchTagsList = searchedInfo.split(" ")
-        const searchResults = searchTagsList.map(one => projectArray.filter(each => each.Categories.includes(one)))
-        const finalResults = []
-        const enhancedResults = searchResults.map(queriedProjectLists => queriedProjectLists.filter(project => finalResults.push(project)))
-        if(finalResults.length > 0 && queriedList.length == 0){
-            setQueriedList(finalResults)
-            console.log(finalResults)
-        }
-    }
-    
-    if(selection === "description"){
-        const results = projectArray.filter(project => {return project.Description.includes(searchedInfo)})
-        if(results.length > 0 && queriedList.length == 0){
-            setQueriedList(results)
-            console.log(results)
-        }
-    }
 
     return(
         <>
         <Navbar />
         <h1 className={styles.textHead} style={{margin: "0"}}>Search Results</h1>
         <br />
-        {queriedList.map((project, index) => 
-            <Preview 
-                    id={projectIdArray[index]}
-                    project={project.Project_Title}
-                    description={project.Description}
-                    creator={project.Creator}
-                    categories={project.Categories}
-                />
+        {queriedList.map(project => 
+            <Preview
+                id={project.ref.id}
+                project={project.data.Project_Title}
+                description={project.data.Description}
+                creator={project.data.Creator}
+                categories={project.data.Categories}
+            />
         )}
-        <br />
         </>
     )
 }
