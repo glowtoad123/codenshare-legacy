121d0e7966db8b594a0957865e34607db25c9725
Alonzo Austin <alonzo6546@outlook.com>
2020-12-15 15:23:29 -0500

    working on new ui

--- pages/project.js
+++ pages/project.js
@@ -91,22 +91,16 @@ export default function Project({id}) {
                 <h1 className={styles.displaytitle}>
                     <strong>{projectData.Project_Title}</strong>
                 </h1>
-                {yourKey === receivedKey && projectData && <div>
+                <p className={styles.description}><strong>{projectData.Description}</strong></p>
+                {yourKey === receivedKey && projectData && <div className={styles.control}>
                     <Link href={`/update?title=${id}`}><a>
-                        <img 
-                            id={projectData.Id}
-                            title={projectData.description} 
-                            name={projectData.Project_Title} 
-                            className={styles.edit} 
-                            src='/edit.svg'
-                        />
+                        <svg id={styles.svg} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
+                          <path fill-rule="evenodd" d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/>
+                        </svg>
                     </a></Link>
-                    <img 
-                        name={projectData.Project_Title} 
-                        src="/delete.svg" 
-                        className={styles.delete} 
-                        onClick={deleteProject}
-                    />
+                        <svg id={styles.svg} onClick={deleteProject} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+                          <path onClick={deleteProject} fill-rule="evenodd" d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5a.5.5 0 0 0-1 0v7a.5.5 0 0 0 1 0v-7z"/>
+                        </svg>
                 </div>}
                 <div className={styles.inquisite}>
                     <svg className={styles.withininquisite} id="inquisiting" width="3em" height="3em" viewBox="0 0 16 16" class="bi bi-eye-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
@@ -119,15 +113,14 @@ export default function Project({id}) {
                 <br />
                 <Link href={`/account?title=${projectData.Creator}`}><a className={styles.creatorName}><strong>{projectData.Creator}</strong></a></Link>
                 <br />
-                <a className={styles.respository} href={projectData.Repository}>{projectData.Repository}</a>
+                <a className={styles.repository} href={projectData.Repository}>{projectData.Repository}</a>
                 <br />
                 {linkList.length > 0 && linkList.map(each => 
                     <a 
-                        className={styles.respository} 
+                        className={styles.repository} 
                         href={each}>{each}
                     </a>
                 ) }
-                <p className={styles.description}><strong>{projectData.Description}</strong></p>
                 <br />
                 <h1 className={styles.textHead}><strong>Roadmap</strong></h1>
                 <br />
